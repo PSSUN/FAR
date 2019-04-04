8cf59c3782cef8b4523212479f3df91189fb502d
Peisen Sun <sps@snnu.edu.cn>
2019-04-05 00:43:16 +0800

    Add files via upload

---
+++ code_specification.py
@@ -0,0 +1,38 @@
+# All scripts should be written based on python3
+# All formats must comply with python PEP-8
+
+# Except for special circumstances,
+# it is not allowed to submit a program that does not comply with the format specified by this script.
+
+# All scripts in the FAR are command line based programs,
+# so the argparse package must be imported,
+# other versions of the package for command line input are not allowed.
+
+import argparse
+# Blank line
+import otrher_packages
+
+
+# The naming of functions must be in lowercase, with _ between words as spaces.
+def function_1():
+    # The naming of variable must be in lowercase, with _ between words as spaces.
+    # NOTE: Hump ​​nomenclature is not allowed!
+    do_something_1 = 'do_something'
+
+
+def function_2():
+    do_something_2 = 'do_something'
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # dest parameters is necessary, content must be in lowercase
+    parser.add_argument('-parameters', dest='parameters', help='Instructions for the parameters must be filled in here')
+    args = parser.parse_args()
+    function_1()
+    function_2()
+    print('DONE!')
+
+
+if __name__ == '__main__':
+    main()
